0b60d0a185579d827c05192757ed537fce45717d
rodriggomartins <rodriggodev@outlook.com>
2025-01-18 23:13:24 -0300

    arrumei a valdicao

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -3,6 +3,6 @@ package request
 type UserRequest struct {
 	Name     string `json:"name" binding:"required,min=4,max=100"`
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6,containsAny=@#$%*&!"`
+	Password string `json:"password" binding:"required,min=6,containsany=@#$%*&!"`
 	Age      int8   `json:"age" binding:"required,min=1,max=140"`
 }
